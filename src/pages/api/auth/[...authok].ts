e3f1219328015366c67e208b48ecb195c8806e47
newtalentxp <newtalentxp@126.com>
2023-04-13 00:01:16 +0800

    fix

--- src/pages/api/auth/[...authok].ts
+++ src/pages/api/auth/[...authok].ts
@@ -3,7 +3,7 @@ import { handleAuth } from '@authok/nextjs-authok';
 export default handleAuth({
   onError(req, res, error) {
     // Add your own custom error handling
-    console.log('onError fuck');
+    console.log('onError fuck', error);
     res.status(error.status || 400).end();
   }
 });
